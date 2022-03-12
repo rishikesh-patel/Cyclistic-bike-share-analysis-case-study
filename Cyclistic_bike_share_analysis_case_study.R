344e941b57bfbcc88e87e01de174e9afc562aab0
Rishikesh <53640666+rishikesh-patel@users.noreply.github.com>
2022-03-12 18:56:30 +0530

    Update Cyclistic_bike_share_analysis_case_study.R

--- Cyclistic_bike_share_analysis_case_study.R
+++ Cyclistic_bike_share_analysis_case_study.R
@@ -7,9 +7,8 @@ library(scales)
 library(dplyr)
 library(tidyverse)  #helps wrangle data
 library(lubridate)  #helps wrangle date attributes
-library(ggplot2)#helps visualize data
-getwd() #displays your working directory
-setwd("/Users/rkpre/Documents/Case_study_1")
+
+#loading all datasets
 td1 <- read.csv("202004-divvy-tripdata.csv")
 td2 <- read.csv("202005-divvy-tripdata.csv")
 td3 <- read.csv("202006-divvy-tripdata.csv")
@@ -23,32 +22,36 @@ td10 <- read.csv("202101-divvy-tripdata.csv")
 td11 <- read.csv("202102-divvy-tripdata.csv")
 td12 <- read.csv("202103-divvy-tripdata.csv")
 
+#Combining all datasets and assigning to (trip_data_fy_2021)
 trip_data_fy_2021 <- rbind(td1, td2, td3, td4, td5, td6, td7, td8, td9, td10, td11, td12)
 
+#Removing any empty rows or columns present and assigning to (new_trip_data_fy_2021)
 new_trip_data_fy_2021 <- remove_empty(trip_data_fy_2021, which=c("rows", "cols"))
 
-new_trip_data_fy_2021 <- na.omit(trip_data_fy_2021)
+#omitting NA values in the entire data frame
+new_trip_data_fy_2021 <- na.omit(new_trip_data_fy_2021)
 
+#Removing duplicates and assigning to (new_trip_data_fy_2021_no_dups)
 new_trip_data_fy_2021_no_dups <- new_trip_data_fy_2021[!duplicated(new_trip_data_fy_2021$ride_id), ]
 
-new_trip_data_fy_2021_no_dups %>%
-  count(rideable_type)
-
-trip_data_fy_2021 %>% 
-  count(day_of_week)
-
+#assigning (new_trip_data_fy_2021_no_dups) dataset to case_study_1
 case_study_1 <- new_trip_data_fy_2021_no_dups
 
+#creating a column called (ride_length)
 case_study_1 <- case_study_1 %>% 
   mutate (ride_length = difftime(ended_at,started_at, units = "mins"))
 case_study_1
 
+#creating a column called (weekday)
 case_study_1 <- case_study_1 %>% 
   mutate(weekday=strftime(case_study_1$ended_at, "%A"))
 
+
+#creating a columns called (date, month, date, year)
 case_study_1$date <- as.Date(case_study_1$started_at)
 case_study_1$month <- format(as.Date(case_study_1$started_at),"%m")
-case_study_1$Datd <- format(as.Date(case_study_1$started_at),"%d")
+case_study_1$Date <- format(as.Date(case_study_1$started_at),"%d")
 case_study_1$year <- format(as.Date(case_study_1$started_at),"%Y")
 
-write.csv(case_study_1,"C:\\Users\\rkpre\\Documents\\case_study_1.csv")
+# dataset for visualization on Tableau
+write.csv(case_study_1, file = "case_study_1", row.names = FALSE)
